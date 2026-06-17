50548758a2726e4bde720de982b7ab3de606ff0a
syeda sakina shahzadi bukhari <sakina0001w@gmail.com>
2026-06-17 16:04:31 +0500

    Initial commit

---
+++ q2.cpp
@@ -0,0 +1,12 @@
+/* print numbers from 10 down to 1 in reverse order */
+#include<iostream>
+using namespace std;
+int main(){
+    for(int i=10;i>=1;i--)
+    {
+        cout<<i<<endl;
+
+
+    }
+    return 0;
+}
