2f0a4421240d15204e120fb23850a8607e9c68d9
syeda sakina shahzadi bukhari <sakina0001w@gmail.com>
2026-07-01 12:51:50 +0500

    update

--- q9.py
+++ q9.py
@@ -1,1 +1,6 @@
-calculate and print the factorial of a given number
+#calculate and print the factorial of a given number
+factorial = 1
+num = int(input("enter number "))
+for i in range(1,num+1):
+    factorial *=i
+print(factorial)
