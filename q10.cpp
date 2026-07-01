f2af413b56f87b73b3fcbcb60f24dccaa505695c
syeda sakina shahzadi bukhari <sakina0001w@gmail.com>
2026-07-01 13:19:02 +0500

    update

--- q10.cpp
+++ q10.cpp
@@ -1,1 +1,17 @@
-find and print theproduct of all digits of a given number 
+//find and print the product of all digits of a given number 
+#include<iostream>
+#include<iomanip>
+#include<cmath>
+using namespace std;
+int main(){
+    int num,product=1;
+
+    cout<<"enter a number ";
+    cin>>num;
+    for(int i=1;i<=num;i++)
+    {
+        product *=i;
+    }
+    cout<<product;
+    return 0;
+}
--- q10.py
+++ q10.py
@@ -1,1 +1,7 @@
-find and print theproduct of all digits of a given number 
+#find and print the product of all digits of a given number 
+product =  1
+num = int(input("enter number"))
+for i in range(1,num+1):
+    product *=i
+
+print (product)
